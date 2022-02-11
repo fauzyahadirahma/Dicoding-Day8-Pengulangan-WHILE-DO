b25cad24ab527e98ca61683c5c3ba9734ffdfd9d
Fauzyah Hadirahma <97279376+fauzyahadirahma@users.noreply.github.com>
2022-02-12 01:17:28 +0700

    12 Feb 2022

---
+++ WHILEdo.c
@@ -0,0 +1,20 @@
+/* File         : WHILEdo.c */
+/* Penulis      : Fauzyah Hadirahma, email faubelajar@gmail.com */
+/* Deskripsi    : print "Saya senang" sebanyak 3 kali */
+
+#include <stdio.h>
+int main () 
+{
+    /* KAMUS */
+    int i = 0;
+    
+    /* ALGORITMA */
+    while (i<3)
+        {
+            /* program diulang selama nilai i<3 */
+            printf ("Saya senang belajar bahasa C \n");
+            i++;
+        }
+    
+    return 0;
+}
